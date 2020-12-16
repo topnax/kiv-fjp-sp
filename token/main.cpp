c0f4ef113bc8839e9a0bd6d8bed262a09c1f2665
Stanislav Král <stanislav.kral3@gmail.com>
2020-12-16 09:36:00 +0100

    Added the possibility to pass either stdin or a file as an input to the parser

--- token/main.cpp
+++ token/main.cpp
@@ -5,12 +5,25 @@ extern "C" int parse(FILE* input, FILE* output);
 
 int main(int argc, char* argv[])
 {
-    FILE* fp = fopen("../samples/struct.cmm", "r");
-    if (!fp) {
-        std::cerr << "Cannot open file" << std::endl;
-        return -1;
+    FILE* fp;
+    if (argc == 2) {
+        // file name provided
+        std::cout << "Opening file " << argv[1] << std::endl;
+        fp = fopen(argv[1], "r");
+
+        if (!fp) {
+            // could not open the the file
+            std::cerr << "Cannot open file " << std::endl;
+            return -1;
+        }
+
+        // parse the provided file
+        parse(fp, stdout);
+
+    } else {
+        // no file provided, parse stdin
+        parse(stdin, stdout);
     }
 
-    parse(fp, stdout);
     return 0;
 }
